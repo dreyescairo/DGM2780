b5541f61f69bb46716fca0b43143252ad00b4e90
David Reyes-Cairo(SurfaceBook) <dreyescairo@gmail.com>
2017-08-29 08:50:03 -0600

    Added javascript to mimic bootstrap 3 affix navbar.

--- Project_1/js/main.js
+++ Project_1/js/main.js
@@ -1,4 +1,40 @@
 
+//Affix was dropped from bootstrap 4. This is a workaround for the sticky nav.
+$(document).ready(function() {
+    
+      var toggleAffix = function(affixElement, scrollElement, wrapper) {
+      
+        var height = affixElement.outerHeight(),
+            top = wrapper.offset().top;
+        
+        if (scrollElement.scrollTop() >= top){
+            wrapper.height(height);
+            affixElement.addClass("affix");
+        }
+        else {
+            affixElement.removeClass("affix");
+            wrapper.height('auto');
+        }
+          
+      };
+      
+    
+      $('[data-toggle="affix"]').each(function() {
+        var ele = $(this),
+            wrapper = $('<div></div>');
+        
+        ele.before(wrapper);
+        $(window).on('scroll resize', function() {
+            toggleAffix(ele, $(this), wrapper);
+        });
+        
+        // init
+        toggleAffix(ele, $(window), wrapper);
+      });
+      
+    });
+
+
 
 $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
@@ -13,3 +49,5 @@ $('.carousel').carousel({
     interval: 5000,
     keyboard: true
   })
+
+
